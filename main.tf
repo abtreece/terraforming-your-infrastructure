7b057c8f7e32b3ff1d9d91276a8c2e5b69e55e50
Britt Treece <btreece@bytekinetic.com>
2016-11-09 20:03:57 -0600

    add count, availability_zone and create_before_destroy lifecycle

--- main.tf
+++ main.tf
@@ -3,10 +3,16 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
+  count         = "${var.instance_count}"
   ami           = "${lookup(var.ami, var.region)}"
   instance_type = "${var.instance_type}"
+  availability_zone = "us-east-1a"
 
   root_block_device {
     delete_on_termination = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
