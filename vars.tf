9024981e82b1eb050bfc9ea646388dfaae87e572
abtreece <britt.treece@gmail.com>
2022-02-02 00:06:06 -0600

    Update 01 vars config

--- vars.tf
+++ vars.tf
@@ -1,10 +1,10 @@
 variable "region" { default = "us-east-1" }
 
 variable "ami" {
-  type = "map"
+  type = map
   default = {
-    us-east-1 = "ami-b73b63a0"
-    us-east-2 = "ami-58277d3d"
+    us-east-1 = "ami-0a8b4cd432b1c3063"
+    us-east-2 = "ami-0231217be14a6f3ba"
   }
 }
 
